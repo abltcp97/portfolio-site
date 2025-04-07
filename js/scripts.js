e7b3e02de93dcb0cbb9f3d0abb0b5bc2b4588dce
abltcp97 <abltcp97@gmail.com>
2025-04-07 11:07:26 -0700

    Initial commit of portfolio site

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+    // Visitor Count
+async function updateVisitorCount() {
+    try {
+      const response = await fetch("https://e814xf9d4e.execute-api.us-east-1.amazonaws.com/visits");
+      const count = await response.text();
+      document.getElementById("visitor-count").innerText = count;
+    } catch (err) {
+      console.error("Visitor count failed", err);
+      document.getElementById("visitor-count").innerText = "N/A";
+    }
+  }
+  
+  updateVisitorCount();
+
+});
+
+
+  
